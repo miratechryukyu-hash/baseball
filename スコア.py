7f605f02ec4dcf321a18bcdce28351b6a1393469
miratechryukyu-hash <miratech.ryukyu@gmail.com>
2026-05-10 19:34:12 +0900

    Update player position names to Japanese terms

--- スコア.py
+++ スコア.py
@@ -30,7 +30,7 @@ st.session_state.member_df = edited_df
 # --- 2. オーダー作成 ---
 st.header("2. 本日のオーダー作成")
 member_list = st.session_state.member_df["名前"].tolist()
-positions = ["投手", "捕手", "一塁手", "二塁手", "三塁手", "遊撃手", "左翼手", "中堅手", "右翼手", "DH"]
+positions = ["ピッチャー", "キャッチャー", "ファースト", "セカンド", "サード", "ショート", "レフト", "センター", "ライト", "DH"]
 
 order_data = []
 cols = st.columns(3) # 3列で表示
